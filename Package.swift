e1c33df1eb82372cc0cb98f6124a763cb4589e17
Francesco Vezzoli <vezzfra@gmail.com>
2024-07-30 10:00:18 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -33,9 +33,9 @@ let package = Package(
             ]
         ),
         .binaryTarget(
-              name: "GoogleInteractiveMediaAds",
-              url: "https://imasdk.googleapis.com/downloads/ima/ios/GoogleInteractiveMediaAds-ios-v3.23.0.zip",
-              checksum: "6fa5ad05c4ab85d74b8aad5fdace8a069f3dbd1eb820496bc04df7aeda0cd5e0"
+            name: "MuxStatsGoogleIMAPluginTVOS",
+            url: "https://github.com/muxinc/mux-stats-google-ima/releases/download/v0.13.0/MuxStatsGoogleIMAPluginTVOS.xcframework.zip",
+            checksum: "bb71f2120ebff9160f8aeae82d7a7c19ddde3e396c42852d01a04ae2f5fda6e8"
         )
     ]
 )
