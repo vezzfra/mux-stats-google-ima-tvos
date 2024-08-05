ea2ba07395dfcb9b7e2ce9eb83407df5265428fa
Francesco Vezzoli <vezzfra@gmail.com>
2024-08-05 10:20:37 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
             name: "MuxStatsGoogleIMAPluginTVOS",
             targets: [
                 "MuxStatsGoogleIMAPluginTVOS",
+                "GoogleInteractiveMediaAdsTVOS"
             ]
         ),
     ],
@@ -25,6 +26,11 @@ let package = Package(
             name: "MuxStatsGoogleIMAPluginTVOS",
             url: "https://github.com/muxinc/mux-stats-google-ima/releases/download/v0.13.0/MuxStatsGoogleIMAPluginTVOS.xcframework.zip",
             checksum: "bb71f2120ebff9160f8aeae82d7a7c19ddde3e396c42852d01a04ae2f5fda6e8"
+        )
+        .binaryTarget(
+              name: "GoogleInteractiveMediaAdsTVOS",
+              url: "https://imasdk.googleapis.com/downloads/ima/tvos/GoogleInteractiveMediaAds-tvos-v4.13.0.zip",
+              checksum: "6fa5ad05c4ab85d74b8aad5fdace8a069f3dbd1eb820496bc04df7aeda0cd5e0"
         )
     ]
 )
